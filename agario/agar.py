7c1d16caf9a4e58255ef51f0d5228dda4200cd56
steevenakintilo <72024403+steevenakintilo@users.noreply.github.com>
2021-08-29 12:11:27 +0200

    A SICK GAME

--- agario/agar.py
+++ agario/agar.py
@@ -10,6 +10,8 @@ import pygame
 from random import randint
 import sys
 from os import system
+import pygame.freetype 
+
 def random_str(n1,n2):
     x = randint(n1,n2)
     return (x)
@@ -107,12 +109,18 @@ def display_allg(screen,x,y,a,b,c,size):
     bsize = randint(1,size)
     pygame.draw.circle(screen, (a, b, c), (x, y), bsize)
 
+def get_listpos(list,nbr):
+    for i in range(len(list)):
+        if list[i] == nbr:
+            return (i)
+            break
 def main_loop():
     brown = (150,75,0)
     green = (0,128,0)
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    GAME_FONT = pygame.freetype.Font("font.ttf", 24)
     #car = Car("car.png")
     X = SCREEN_WIDTH/2
     Y = SCREEN_HEIGHT/2
@@ -128,6 +136,8 @@ def main_loop():
     move2 = 0
     nove1 = 0
     nove2 = 0
+    pove1 = 0
+    pove2 = 0
     bx = 0
     gx = 0
     d = 0
@@ -141,19 +151,28 @@ def main_loop():
     f = 0
     size = 20
     iasize = 20
+    iasize2 = 20
     restart = 0
     iarestart = 0
+    iarestart2 = 0
+    score = [1,1,1]
+    name = ["Moi ","Ia ","Ia2 "]
     iax = randint(100,1820)
     iay = randint(100,1180)
+    iax2 = randint(100,1820)
+    iay2 = randint(100,1180)
     for i in range(200):
         y.append(d)
-        pos_x.append(randint(50,1870))
-        pos_y.append(randint(50,1250))
+        pos_x.append(randint(100,1800))
+        pos_y.append(randint(100,1180))
         color1.append(randint(0,255))
         color2.append(randint(0,255))
         color3.append(randint(0,255))
         d = d + 100
     color = (217,225,229)
+    textsize = 14
+    textsize2 = 14
+    textsize3 = 14
     while running:
         i = i + 1
         if i % 10 ==0:
@@ -172,11 +191,21 @@ def main_loop():
         psy = py + move2
         piax = iax + nove1
         piay = iay + nove2
+        piax2 = iax2 + pove1
+        piay2 = iay2 + pove2
         s = size - 20
         ais = iasize - 20
         slow = size/500
-        wlow = iasize/500
-        print(int(iax+nove1),int(iay+nove2),pos_x[0],pos_y[0])
+        wlow = 0
+        score[0] = size
+        score[1] = iasize
+        score[2] = iasize2
+        score.sort()
+        
+        NAME_FONT = pygame.freetype.Font("font.ttf", textsize)
+        NAME_FONT2 = pygame.freetype.Font("font.ttf", textsize2)
+        NAME_FONT3 = pygame.freetype.Font("font.ttf", textsize3)
+        #print(int(iax+nove1),int(iay+nove2),pos_x[0],pos_y[0])
         if pressed_keys[ord("z")] and py+move2-s > 24:
             move2 = move2 - 1 + slow
         if pressed_keys[ord("q")] and px+move1-s > 20:
@@ -194,7 +223,31 @@ def main_loop():
             nove2 = nove2 - 1 - wlow
         if iay+nove2 < 978:
             nove2 = nove2 + 1 + wlow
+        
+        if iax2+pove1 > 1895:
+            pove1 = pove1 - 1
+        if iax2+pove1 < 20:
+            pove1 = pove1 + 1
+        if iay2+pove2 > 24:
+            pove2 = pove2 - 1
+        if iay2+pove2 < 978:
+            pove2 = pove2 + 1
         #pygame.draw.rect(screen, color, pygame.Rect(0, 0, 10000, 10000))
+        if size >= 490:
+            system("clear")
+            print("You win")
+            quit()
+        if iasize >= 490:
+            system("clear")
+            print("Ia1 win")
+            quit()
+        if iasize2 >= 490:
+            system("clear")
+            print("Ia2 win")
+            quit()
+        name[get_listpos(score,size)] = "Moi  "
+        name[get_listpos(score,iasize)] = "Ai  "
+        name[get_listpos(score,iasize2)] = "Ai2  "
         if iasize < 490:
             if iasize <= size:
                 if int(iax+nove1) < int(pos_x[randint(0,0)]):
@@ -225,36 +278,137 @@ def main_loop():
                     #nove2 = int(pos_y[randint(0,2)]) - iay
                 if int(iay+nove2) > int(psy):
                     nove2 = nove2 - 0.3 + wlow
+        
+        if iasize2 < 490:
+            if iasize2 <= iasize:
+                if int(iax2+pove1) < int(pos_x[randint(1,1)]):
+                    pove1 = pove1 + 1
+                    #nove1 = int(pos_x[randint(0,2)]) - iax
+                if int(iax2+pove1) > int(pos_x[randint(1,1)]):
+                    pove1 = pove1 - 1
+                    #nove1 = int(pos_x[randint(0,2)]) - iax
+                if int(iay2+pove2) < int(pos_y[randint(1,1)]):
+                    pove2 = pove2 + 1
+                    #nove2 = int(pos_y[randint(0,2)]) - iay
+                if int(iay2+pove2) > int(pos_y[randint(1,1)]):
+                    pove2 = pove2 - 1
+                    #nove2 = int(pos_y[randint(0,2)]) - iay
+                if int(iay2+pove2) == int(pos_y[randint(1,1)]):
+                    pos_x[1] = randint(50,1250)
+                if int(iax2+pove1) == int(pos_x[randint(1,1)]):
+                    pos_y[1] = randint(50,1870)
+            if iasize2 >= iasize:
+                if int(iax2+pove1) < int(piax):
+                    pove1 = pove1 + 0.3
+                    #nove1 = int(pos_x[randint(0,2)]) - iax
+                if int(iax2+pove1) > int(piax):
+                    pove1 = pove1 - 0.3
+                    #nove1 = int(pos_x[randint(0,2)]) - iax
+                if int(iay2+pove2) < int(piay):
+                    pove2 = pove2 + 0.3
+                    #nove2 = int(pos_y[randint(0,2)]) - iay
+                if int(iay2+pove2) > int(piay):
+                    pove2 = pove2 - 0.3
         if psx - size <= piax and psx + size >= piax and psy - size <= piay and psy + size >= piay and size > iasize:
             size = size + iasize
+            textsize = textsize + iasize
             iarestart = 0
             if iarestart == 0:
-                iax = randint(100,1820)
+                iax = randint(100,1800)
                 iay = randint(100,1180)
                 iarestart = 1
             iasize = 20
-        if psx - size <= piax and psx + size >= piax and psy - size <= piay and psy + size >= piay and size < iasize:
-            iasize = iasize + size
+            textsize2 = 14
+        if psx - size <= piax2 and psx + size >= piax2 and psy - size <= piay2 and psy + size >= piay2 and size > iasize2:
+            size = size + iasize2
+            textsize = textsize + iasize2
             restart = 0
             if restart == 0:
-                px = randint(50,1870)
-                py = randint(50,1230)
+                iax2 = randint(100,1800)
+                iay2 = randint(100,1180)
+                restart = 1
+            iasize2 = 20
+            textsize3 = 14
+        if psx - size <= piax and psx + size >= piax and psy - size <= piay and psy + size >= piay and size < iasize:
+            iasize = iasize + size
+            textsize2 = textsize2 + size
+            restart = 0
+            if restart == 0:
+                px = randint(100,1800)
+                py = randint(100,1180)
                 restart = 1
             size = 20
+            textsize = 14
+        if psx - size <= piax2 and psx + size >= piax2 and psy - size <= piay2 and psy + size >= piay2 and size < iasize2:
+            iasize2 = iasize2 + size
+            textsize3 = textsize3 + size
+            restart = 0
+            if restart == 0:
+                px = randint(100,1800)
+                py = randint(100,1180)
+                restart = 1
+            size = 20
+            textsize = 14
+        if piax - iasize <= piax2 and piax + iasize >= piax2 and piay - iasize <= piay2 and piay + iasize >= piay2 and iasize2 > iasize:
+            iasize2 = iasize2 + iasize
+            textsize3 = textsize3 + iasize
+            iarestart2 = 0
+            if iarestart2 == 0:
+                iax = randint(100,1800)
+                iay = randint(100,1180)
+                iarestart2 = 1
+            iasize = 20
+            textsize2 = 14
+        if piax - iasize <= piax2 and piax + iasize >= piax2 and piay - iasize <= piay2 and piay + iasize >= piay2 and iasize > iasize2:
+            iasize = iasize + iasize2
+            iarestart2 = 0
+            textsize2 = textsize2 + iasize2
+            if iarestart2 == 0:
+                iax2 = randint(100,1800)
+                iay2 = randint(100,1180)
+                iarestart2 = 1
+            iasize2 = 20
+            textsize3 = 14
+        
+        if piax - iasize <= piax2 and piax + iasize >= piax2 and piay - iasize <= piay2 and piay + iasize >= piay2 and iasize < iasize2:
+            iasize2 = iasize2 + iasize
+            iarestart = 0
+            if iarestart == 0:
+                iax = randint(100,1800)
+                iay = randint(100,1180)
+                iarestart = 1
+            iasize = 20
+            textsize2 = 14
         for i in range(200):
             if psx - size <= int(pos_x[i]) and psx + size >= int(pos_x[i]) and psy - size <= int(pos_y[i]) and psy + size >= int(pos_y[i]) and size < 490:
                 size = size + 1
-                pos_x[i] = randint(50,1870)
-                pos_y[i] = randint(50,1250)
+                textsize = textsize + 1
+                pos_x[i] = randint(100,1800)
+                pos_y[i] = randint(100,1180)
             if piax - iasize <= int(pos_x[i]) and piax + iasize >= int(pos_x[i]) and piay - iasize <= int(pos_y[i]) and piay + iasize >= int(pos_y[i]) and iasize < 490:
                 iasize = iasize + 1
-                pos_x[i] = randint(50,1870)
-                pos_y[i] = randint(50,1250)
+                textsize2 = textsize2 + 1
+                pos_x[i] = randint(100,1800)
+                pos_y[i] = randint(100,1180)
+            if piax2 - iasize2 <= int(pos_x[i]) and piax2 + iasize2 >= int(pos_x[i]) and piay2 - iasize2 <= int(pos_y[i]) and piay2 + iasize2 >= int(pos_y[i]) and iasize2 < 490:
+                iasize2 = iasize2 + 1
+                textsize3 = textsize3 + 1
+                pos_x[i] = randint(100,1800)
+                pos_y[i] = randint(100,1180)
             pygame.draw.rect(screen, color, pygame.Rect(0, y[i], 12000, 2))
             pygame.draw.rect(screen, color, pygame.Rect(y[i], 0, 2, 12000))
             pygame.draw.circle(screen, (color1[i], color2[i], color3[i]), (pos_x[i], pos_y[i]), 10)
+        print(psx,piax,piax2)
         pygame.draw.circle(screen, (color1[i], color2[i], color3[i]), (px+ move1, py + move2), size)
+        NAME_FONT.render_to(screen, (int(psx-10-textsize/3), int(psy-10-textsize/5)), "Moi", (255,255,255))
         pygame.draw.circle(screen, (color1[10], color2[20], color3[30]), (iax + nove1, iay + nove2), iasize)
+        NAME_FONT2.render_to(screen, (int(piax-10-textsize2/3), int(piay-10-textsize2/5)), "IA1", (255,255,255))
+        pygame.draw.circle(screen, (color1[11], color2[21], color3[31]), (iax2 + pove1, iay2 + pove2), iasize2)
+        NAME_FONT3.render_to(screen, (int(piax2-10-textsize3/3), int(piay2-10-textsize3/5)), "IA2", (255,255,255))
+        GAME_FONT.render_to(screen, (20, 25), "Classement: ",(0,0,0))
+        GAME_FONT.render_to(screen, (20, 45), " 1 " + name[2] + str(score[2]), (0,0,0))
+        GAME_FONT.render_to(screen, (20, 65), "2 " + name[1] + str(score[1]), (0,0,0))
+        GAME_FONT.render_to(screen, (20, 85), "3 " + name[0] + str(score[0]), (0,0,0))
         pygame.display.flip()
         
 system("clear")
